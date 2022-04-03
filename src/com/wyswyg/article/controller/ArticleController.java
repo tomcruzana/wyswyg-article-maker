a529ce606458bf68fbb04298fc61f0e8690cc775
tomcruzana <tomcruzana@ymail.com>
2022-04-03 01:21:20 -0700

    added views

--- src/com/wyswyg/article/controller/ArticleController.java
+++ src/com/wyswyg/article/controller/ArticleController.java
@@ -12,11 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 public class ArticleController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		String x = req.getParameter("testdata");
+		System.out.println(x);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		String x = req.getParameter("testdata");
+		System.out.println(x);
 	}
 }
